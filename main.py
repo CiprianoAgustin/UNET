59251c8dc291eac96f7b238f3a5de7de7dd87aa8
Agustín CIpriano <agustincipriano@hotmail.com>
2018-11-03 17:55:34 -0300

    Test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from model import *
 from data import *
+import sys
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -11,12 +12,12 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/lung/train','image','label',data_gen_args,save_to_dir = None)
+myGene = trainGenerator(2,str(sys.argv[1]),'image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_lung.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data/lung/test")
+testGene = testGenerator(str(sys.argv[2]))
 results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/lung/test",results)
+saveResult(str(sys.argv[2],results))
