6d28044dc51890355e3fc0985b99e987b6109b73
Agustín CIpriano <agustincipriano@hotmail.com>
2018-11-05 20:49:55 -0300

    Cambios

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ myGene = trainGenerator(2,str(sys.argv[1]),'image','label',data_gen_args,save_to
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_lung.hdf5', monitor='loss',verbose=1, save_best_only=True)
-#model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
 testGene = testGenerator(str(sys.argv[2]), num_image=len(os.listdir(str(sys.argv[2]))))
 results = model.predict_generator(testGene,len(os.listdir(str(sys.argv[2]))),verbose=1)
